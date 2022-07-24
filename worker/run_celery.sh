b1344d467ce28a3cf15d52583d10be0c18fa994e
palawer <guillemzz@gmail.com>
2022-07-24 09:59:05 +0200

    Update run_celery.sh

--- worker/run_celery.sh
+++ worker/run_celery.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "$(date +'%Y-%m-%dT%H:%M:%S') Starting worker"
+
 su -m celery_user -c "celery -A tasks worker --pool=gevent --concurrency=20 --loglevel=INFO --logfile=/app/logs/celery.log"
